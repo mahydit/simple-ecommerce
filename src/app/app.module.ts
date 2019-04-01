f54f0b5207f1cee5101e65f3431af06b24e348a0
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-04-02 00:32:59 +0200

    register created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { NavigationlinksComponent } from './components/navigationlinks/navigationlinks.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { SingleproductComponent } from './components/singleproduct/singleproduct.component';
+import { LoginComponent } from './components/login/login.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LogoutComponent,
+    NavigationlinksComponent,
+    WishlistComponent,
+    SingleproductComponent,
+    LoginComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
