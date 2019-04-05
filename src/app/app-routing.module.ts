12d4a224fe30a625863ecc6317e2d6224f93c3ad
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-04-05 11:42:04 +0200

    add components routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CartComponent } from './components/cart/cart.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   { path: 'cart', component: CartComponent},
-  { path: 'wishlist', component: WishlistComponent}
+  { path: 'wishlist', component: WishlistComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegisterComponent},
 ];
 
 @NgModule({
