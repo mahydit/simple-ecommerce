f702b273a63c2df46bbc046bcefd6b7e9acab770
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-04-05 10:23:59 +0200

    create registeration form

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+// import { MustMatch } from '../../../helpers/must-match.validator';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm : FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      'name': new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      'email': new FormControl(null,[Validators.required, Validators.email,]),
+      'password': new FormControl(null,[Validators.required, Validators.minLength(8)]),
+      'confirmPassword': new FormControl(null,[Validators.required,]),
+    }
+    // , {
+      // validator: MustMatch('password', 'confirmPassword'),
+  // }
+  );
+  }
+
+  onSubmit(){
+    console.log(this.registerForm);
+    console.log(JSON.stringify(this.registerForm.value));   
   }
 
 }
+
