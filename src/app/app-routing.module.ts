75541eb975c6bbaae7a6034daa02aa45b0ed429d
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-04-05 11:28:25 +0200

    add cart and wishlist to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './components/cart/cart.component';
+import { WishlistComponent } from './components/wishlist/wishlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cart', component: CartComponent},
+  { path: 'wishlist', component: WishlistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
