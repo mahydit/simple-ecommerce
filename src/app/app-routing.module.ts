b90a467f8315ed1fc9f32ba2c835333ec737182c
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-04-06 12:40:36 +0200

    add notfound component and single product route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { CartComponent } from './components/cart/cart.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { ProductlistComponent } from './components/productlist/productlist.component';
+import { SingleproductComponent } from './components/singleproduct/singleproduct.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
 
 const routes: Routes = [
   { path: 'cart', component: CartComponent},
   { path: 'wishlist', component: WishlistComponent},
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegisterComponent},
+  { path: 'products', component: ProductlistComponent},
+  { path: '', component: ProductlistComponent},
+  { path: 'product/:id', component: SingleproductComponent},
+  { path: '404', component: NotfoundComponent}
 ];
 
 @NgModule({
