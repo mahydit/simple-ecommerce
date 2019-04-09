9e83ae82fd2df97f411d8cfe4c879ebdae3362c8
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-04-09 15:31:02 +0200

    create auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  isAuthenticated: boolean = false;
+
+  constructor() { }
+
+  setIsAuthenticated(state: boolean): void{
+    this.isAuthenticated = state;
+  }
+
+  getIsAuthenticated(): boolean{
+    return this.isAuthenticated;
+  }
+}
