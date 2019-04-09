6e131e9a51c635f4e120596cc670be0624f7b65b
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-04-09 15:31:23 +0200

    use auth service with stream service

--- src/app/services/stream.service.ts
+++ src/app/services/stream.service.ts
@@ -1,16 +1,18 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { CartService } from './cart.service';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StreamService {
-  private name: BehaviorSubject<string> = new BehaviorSubject('');
+  private authenticated: BehaviorSubject<string> = new BehaviorSubject('');
   private cartCount: BehaviorSubject<string> = new BehaviorSubject('');
 
-  constructor(private cartService: CartService) {
+  constructor(private cartService: CartService, private auth: AuthService) {
     this.cartCount.next(this.cartService.calculateTotalItems().toString());
+    this.authenticated.next(this.auth.getIsAuthenticated().toString());
   }
 
   setCartCount(val?: string): void {
@@ -21,4 +23,11 @@ export class StreamService {
     return this.cartCount.asObservable();
   }
 
+  setAuthenticated(val?: string): void {
+    this.authenticated.next(val);
+  }
+
+  getAuthenticated(){
+    return this.authenticated.asObservable();
+  }
 }
