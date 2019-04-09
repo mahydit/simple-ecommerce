0888a4f4d54655c1d21337a7571b100ef36c6675
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-04-09 22:11:55 +0200

    add logout function

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StreamService } from 'src/app/services/stream.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { CartService } from 'src/app/services/cart.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -12,16 +14,30 @@ export class HeaderComponent implements OnInit {
   show : boolean = false;
   authenticated: any;
 
-  constructor(private streamService: StreamService, private auth: AuthService){
+  constructor(
+    private streamService: StreamService, 
+    private auth: AuthService,
+    private cartService: CartService,
+    private router: Router
+  ){
     this.streamService.getCartCount().subscribe(res => this.cartCount = res);
     this.streamService.getAuthenticated().subscribe(res => this.authenticated = res);
     this.authenticated = (this.authenticated =="true");
   }
 
   ngOnInit() {
-    if(this.cartCount>=0){
+    if(this.cartCount>0){
       this.show = true;
     }
+  }
+
+  logout(){
+    this.streamService.setAuthenticated("false");
+    this.authenticated = false;
+    localStorage.removeItem("cart");
+    localStorage.removeItem("wishlist");
+    this.streamService.setCartCount(this.cartService.calculateTotalItems().toString());
+    this.router.navigate(['']); 
   }
 
 }
