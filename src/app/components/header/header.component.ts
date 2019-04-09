78fe071732534dd2f0d086e03527e1d54127ade1
Mahydit Mahmoud Anber <Mahydit_anber@outlook.com>
2019-04-09 11:59:50 +0200

    add cart counter to the header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  totalItems : number = 0;
+  show : boolean = false;
 
-  constructor() { }
+  constructor(private cartService : CartService) { }
 
   ngOnInit() {
+    this.totalItems = this.cartService.calculateTotalItems();
+    if(this.totalItems>0){
+      this.show = true;
+    }
   }
 
 }
